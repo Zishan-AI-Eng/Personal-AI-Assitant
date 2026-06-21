fa759b41467680d75173cf9cc5ac4d16c3d80b78
Zeeshan Marwat <Zishan-AI-Eng@users.noreply.github.com>
2026-06-21 17:05:42 +0500

    Updated Profile Data Information

--- core/config.py
+++ core/config.py
@@ -3,20 +3,12 @@ from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 
 load_dotenv()
-
-
 DATABASE_URL = os.getenv("DATABASE_URL")
-
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 def get_llm(temperature: float = 0.1):
-    """
-    Initializes and returns the primary LLM instance.
-
-    """
     return ChatGroq(
         model="llama-3.3-70b-versatile",
         temperature=temperature,
+        groq_api_key=GROQ_API_KEY
     )
-
-
-
